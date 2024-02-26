2852428dc5e36f783b68b17bad226fce0e9fb65a
Reginaldo762 <reginaldogrellmann@gmail.com>
2024-02-26 16:20:48 -0300

    Mais completo

---
+++ ProjFin---Front-End-main/conexao.php
@@ -0,0 +1,23 @@
+<?php
+    $dbHost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "cuidadosos";
+
+    $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    //if($conn->connect_error){
+        //die("Conexão falhou!" . $conn->connect_error);
+    //}
+    //else{
+        //echo "Conexão efetuada com sucesso!";
+    //}
+
+
+
+
+
+
+
+
+?>
